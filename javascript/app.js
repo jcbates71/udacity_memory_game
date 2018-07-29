b4015e1e3718521d9f5ed2ac5e43a757d5bb7198
John Bates <waterdeep71@gmail.com>
2018-07-29 14:38:56 -0500

    Completed selectCard()

--- javascript/app.js
+++ javascript/app.js
@@ -2,8 +2,10 @@ const symbolList = ['1', '1', '2', '2', '3', '3', '4', '4', '5', '5', '6', '6', 
 const cardList = ['1-1', '1-2', '1-3', '1-4', '2-1', '2-2', '2-3', '2-4', '3-1', '3-2', '3-3', '3-4', '4-1', '4-2', '4-3', '4-4'];
 var cardLocations;
 var score;
+var selected;
 
 function reset() {
+  selected = new Array();
   setScore(0);
   flipAllCardsToBack();
   shuffleCards();
@@ -36,7 +38,13 @@ function shuffleCards() {
 }
 
 function selectCard(e) {
-
+  var selectedCart = e.target;
+  if (selectCard.classList.contains('card') && !selected.includes(selectCard.id)) {
+    flipCard(selectCard);
+    if (selected.length == 2) {
+      checkSelectedCards();
+    }
+  }
 }
 
 function addEventListeners() {
