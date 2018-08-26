b610800bb69e03a682d43e45fdf4d3ca60a8a406
John Bates <waterdeep71@gmail.com>
2018-08-25 22:48:11 -0500

    Reordered the JavaScript functions

--- javascript/app.js
+++ javascript/app.js
@@ -5,60 +5,17 @@ let score, starCount;
 let startTime, currentTime, myTimer;
 let selected = document.getElementsByClassName('selected');
 
-function reset() {
-  // Wipes out all progress.
-  setScore(0);
-  flipAllCardsToBack();
-  shuffleCards();
-  turnOffGameWinners();
-  stopTimer();
-  document.getElementById('timer').innerText = '0:00';
+function main() {
+  // Starts the game.
+  reset();
+  addEventListeners();
 }
 
-function setScore(newScore) {
-  // Sets the score variable and updates display.
-  score = newScore;
-  document.getElementById('score-number').textContent = score;
-  document.getElementById('score-stars').innerHTML = getStarHtml();
-}
-
-function getStarHtml() {
-  // Calculates the number of starts based on the score and creates the html.
-  if (score < 13) {
-    starCount = 3;
-  } else if (score < 22) {
-    starCount = 2;
-  } else {
-    starCount = 1;
-  }
-  let starHtml = '<span class="good-stars">';
-  for (let i = 1; i < 4; i++) {
-    starHtml += '&#9733;';
-    if (i == starCount) {
-      starHtml += '</span><span class="bad-stars">';
-    }
-  }
-  starHtml += '</span>';
-  return starHtml;
-}
-
-function getStatText(number, units) {
-  // Creates the text for displaying an end-game stat.
-  return number + ' ' + units + (number == 1 ? '' : 's');
-}
-
-function flipAllCardsToBack() {
-  // Flips all cards to their backs by remplacing all card classes with back
-  // and setting each's display text to "?".
-  let cardDisplays = document.getElementsByClassName('card-display');
-  for (let display of cardDisplays) {
-    display.textContent = '?';
-  }
-  let allCards = document.getElementsByClassName('card');
-  for (let card of allCards) {
-    card.classList.remove('selected', 'matched');
-    card.classList.add('back');
-  }
+function addEventListeners() {
+  // Adds all of the game's event listeners.
+  document.getElementById('reset-button').addEventListener('click', reset);
+  document.getElementById('game-board').addEventListener('click', selectCard);
+  document.getElementById('game-board').addEventListener('animationend', finishFlipCard);
 }
 
 function shuffleCards() {
@@ -69,50 +26,6 @@ function shuffleCards() {
     let randomCard = Math.floor(Math.random() * unusedCards.length);
     cardLocations.set(card, unusedCards.splice(randomCard, 1)[0]);
   }
-}
-
-function turnOffGameWinners() {
-  // Removes classes and html added after winning the game.
-  document.getElementById('game-board').classList.remove('board-winner');
-  const GAME_OVER_SCREEN = document.getElementById('game-over');
-  if (GAME_OVER_SCREEN) {
-    document.getElementsByTagName('body')[0].removeChild(GAME_OVER_SCREEN);
-  }
-}
-
-function addGameOver() {
-  // Creates the end game html.
-  const GAME_OVER_PAGE = document.createElement('div');
-  GAME_OVER_PAGE.className = 'end-screen';
-  GAME_OVER_PAGE.id = 'game-over';
-  const GAME_OVER_HEADER = document.createElement('h2');
-  GAME_OVER_HEADER.innerText = 'Congratulations! You won!';
-  GAME_OVER_PAGE.appendChild(GAME_OVER_HEADER);
-  const GAME_OVER_STATS = document.createElement('p');
-  GAME_OVER_STATS.className = 'game-stats';
-  GAME_OVER_STATS.innerHTML = '<span id="move-count"></span> and <span id="star-count"></span>';
-  GAME_OVER_PAGE.appendChild(GAME_OVER_STATS);
-  const PLAY_AGAIN_BUTTON = document.createElement('div');
-  PLAY_AGAIN_BUTTON.className = 'play-again';
-  PLAY_AGAIN_BUTTON.addEventListener('click', reset);
-  const PLAY_AGAIN_TEXT = document.createElement('p');
-  PLAY_AGAIN_TEXT.innerHTML = '<span class="button">Play again?</span>';
-  PLAY_AGAIN_BUTTON.appendChild(PLAY_AGAIN_TEXT);
-  GAME_OVER_PAGE.appendChild(PLAY_AGAIN_BUTTON);
-  document.getElementsByTagName('body')[0].appendChild(GAME_OVER_PAGE);
-  calculateStats();
-}
-
-function calculateStats() {
-  // Adds the stats to the end game screen.
-  document.getElementById('move-count').innerText = getStatText(score, 'move');
-  document.getElementById('star-count').innerText = getStatText(starCount, 'star');
-}
-
-function startTimer() {
-  // Captures the current time and sets the timer for continuous updating.
-  startTime = new Date();
-  myTimer = setInterval(updateTimer, 100);
 }
 
 function selectCard(e) {
@@ -187,15 +100,57 @@ function checkGameOver() {
   }
 }
 
+function flipAllCardsToBack() {
+  // Flips all cards to their backs by remplacing all card classes with back
+  // and setting each's display text to "?".
+  let cardDisplays = document.getElementsByClassName('card-display');
+  for (let display of cardDisplays) {
+    display.textContent = '?';
+  }
+  let allCards = document.getElementsByClassName('card');
+  for (let card of allCards) {
+    card.classList.remove('selected', 'matched');
+    card.classList.add('back');
+  }
+}
+
+function setScore(newScore) {
+  // Sets the score variable and updates display.
+  score = newScore;
+  document.getElementById('score-number').textContent = score;
+  document.getElementById('score-stars').innerHTML = getStarHtml();
+}
+
+function getStarHtml() {
+  // Calculates the number of starts based on the score and creates the html.
+  if (score < 13) {
+    starCount = 3;
+  } else if (score < 22) {
+    starCount = 2;
+  } else {
+    starCount = 1;
+  }
+  let starHtml = '<span class="good-stars">';
+  for (let i = 1; i < 4; i++) {
+    starHtml += '&#9733;';
+    if (i == starCount) {
+      starHtml += '</span><span class="bad-stars">';
+    }
+  }
+  starHtml += '</span>';
+  return starHtml;
+}
+
+function startTimer() {
+  // Captures the current time and sets the timer for continuous updating.
+  startTime = new Date();
+  myTimer = setInterval(updateTimer, 100);
+}
+
 function stopTimer() {
   // Stops the timer for when the game is finished.
   clearInterval(myTimer);
   startTime = false;
-}
-
-function flashGameBoard() {
-  // Game board flashes briefly when the game has been won.
-  document.getElementById('game-board').classList.add('board-winner');
 }
 
 function updateTimer() {
@@ -213,17 +168,62 @@ function updateTimer() {
   }
 }
 
-function addEventListeners() {
-  // Adds all of the game's event listeners.
-  document.getElementById('reset-button').addEventListener('click', reset);
-  document.getElementById('game-board').addEventListener('click', selectCard);
-  document.getElementById('game-board').addEventListener('animationend', finishFlipCard);
+function reset() {
+  // Wipes out all progress.
+  setScore(0);
+  flipAllCardsToBack();
+  shuffleCards();
+  turnOffGameWinners();
+  stopTimer();
+  document.getElementById('timer').innerText = '0:00';
 }
 
-function main() {
-  // Starts the game.
-  reset();
-  addEventListeners();
+function flashGameBoard() {
+  // Game board flashes briefly when the game has been won.
+  document.getElementById('game-board').classList.add('board-winner');
+}
+
+function addGameOver() {
+  // Creates the end game html.
+  const GAME_OVER_PAGE = document.createElement('div');
+  GAME_OVER_PAGE.className = 'end-screen';
+  GAME_OVER_PAGE.id = 'game-over';
+  const GAME_OVER_HEADER = document.createElement('h2');
+  GAME_OVER_HEADER.innerText = 'Congratulations! You won!';
+  GAME_OVER_PAGE.appendChild(GAME_OVER_HEADER);
+  const GAME_OVER_STATS = document.createElement('p');
+  GAME_OVER_STATS.className = 'game-stats';
+  GAME_OVER_STATS.innerHTML = '<span id="move-count"></span> and <span id="star-count"></span>';
+  GAME_OVER_PAGE.appendChild(GAME_OVER_STATS);
+  const PLAY_AGAIN_BUTTON = document.createElement('div');
+  PLAY_AGAIN_BUTTON.className = 'play-again';
+  PLAY_AGAIN_BUTTON.addEventListener('click', reset);
+  const PLAY_AGAIN_TEXT = document.createElement('p');
+  PLAY_AGAIN_TEXT.innerHTML = '<span class="button">Play again?</span>';
+  PLAY_AGAIN_BUTTON.appendChild(PLAY_AGAIN_TEXT);
+  GAME_OVER_PAGE.appendChild(PLAY_AGAIN_BUTTON);
+  document.getElementsByTagName('body')[0].appendChild(GAME_OVER_PAGE);
+  calculateStats();
+}
+
+function calculateStats() {
+  // Adds the stats to the end game screen.
+  document.getElementById('move-count').innerText = getStatText(score, 'move');
+  document.getElementById('star-count').innerText = getStatText(starCount, 'star');
+}
+
+function getStatText(number, units) {
+  // Creates the text for displaying an end-game stat.
+  return number + ' ' + units + (number == 1 ? '' : 's');
+}
+
+function turnOffGameWinners() {
+  // Removes classes and html added after winning the game.
+  document.getElementById('game-board').classList.remove('board-winner');
+  const GAME_OVER_SCREEN = document.getElementById('game-over');
+  if (GAME_OVER_SCREEN) {
+    document.getElementsByTagName('body')[0].removeChild(GAME_OVER_SCREEN);
+  }
 }
 
 document.addEventListener('DOMContentLoaded', main);
